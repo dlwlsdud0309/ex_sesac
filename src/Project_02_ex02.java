42fcfaf75f8eaa5d218ab00bb720017f3b3101c6
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-07-05 21:41:54 +0900

    증감연산자에 대하여, i++는 가지고있는 i값을 먼저 출력 후 그 다음 줄에서 +1된 값이 출력된다

---
+++ src/Project_02_ex02.java
@@ -0,0 +1,15 @@
+
+public class Project_02_ex02 {
+	public static void main(String[] args) {
+		
+		int num1 = 3;
+		int num2 = 5;
+		
+		num1 = num2+2; //5(num2)+2=7의 값이 num1에 대입된 것임
+		System.out.println(num1);
+		
+		num2 +=10; //5(num2)+10=15의 값이 num2에 대입됨
+		System.out.println(num2);
+		
+	}
+}
---
+++ src/Project_02_ex03.java
@@ -0,0 +1,11 @@
+
+public class Project_02_ex03 {
+	public static void main(String[] args) {
+		
+		int i = 3;
+		
+		System.out.println(++i); //여기서 i는 4가 됨
+		System.out.println(i++); //가지고 있는 값을 먼저 출력 후 다음 줄에서 +1됨
+		System.out.println(i);
+	}
+}
