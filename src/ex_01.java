f43c8fa3dbc024b11e3401a7738feccec2387980
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-07-05 21:26:56 +0900

    변수 선언 및 타입에 대하여

--- src/ex_01.java
+++ src/ex_01.java
@@ -2,7 +2,12 @@
 public class ex_01 {
 
 	public static void main(String[] args) {
-
+		int number = 5;
+//		int class = 7; 자바에서 class라는 키워드를 사용하고 있기 때문에 변수명으로 사용할 수 없음
+		int asjkdjals = 5;
+		
+		//정수형 +, - byte short int long
+		//실수형 소수점 float, double
 	}
 
 }
---
+++ src/ex_02.java
@@ -0,0 +1,20 @@
+
+public class ex_02 {
+	public static void main(String[] args) {
+		
+		//1. 논리형 - boolean
+		boolean name1 = true;
+		
+		//2. 문자형
+		char name2 = 'a';
+		
+		//3. 정수형
+		//byte short int long
+		int name3 = 8;
+		
+		//4. 실수형
+		//float double
+		double name4 = 3.14;
+		
+	}
+}
