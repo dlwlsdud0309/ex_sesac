1e5a5f027b43bda46bccd81a1a4f987bb818f05a
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-07-06 14:42:39 +0900

    출력문 사용하기

---
+++ src/Project_03_ex01.java
@@ -0,0 +1,17 @@
+
+public class Project_03_ex01 {
+	public static void main(String[] args) {
+		
+		//1. 출력하기
+		System.out.println("안녕하세요");
+		
+		//2. 변수명 지정하여 변수가 지정한 값 출력하기
+		String name = "이진영";
+		System.out.println(name); //큰따옴표가 없으면 변수로 인식함
+	
+//		System.out.println(name "입니다."); //에러, 연결 위해 +연산자 필요
+		System.out.println(name+"입니다.");
+
+		System.out.print(name+"입니다."); //println과 print
+	}
+}
