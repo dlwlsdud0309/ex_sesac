a47f3707fc687534e776853448c04e508eabd144
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-07-09 13:02:33 +0900

    while과 if문 함께 사용하여 응용

---
+++ src/Practice09_NO8.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Practice09_NO8 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+//		int i = 0;
+//		while(i <= 10) {
+//			System.out.print("점수 입력 : ");
+//			i = sc.nextInt();
+//		}
+//		System.out.println("종료되었습니다.");
+
+		int score = 0;
+		while(score <= 100) {
+			System.out.println("점수 입력 : ");
+			score = sc.nextInt();
+			if(score >= 90) {
+				System.out.println("A학점입니다.");
+//				continue;
+			}else if(score >= 80) {
+				System.out.println("B학점입니다.");
+			}else if(score >= 70) {
+				System.out.println("C학점입니다.");
+			}else {
+				System.out.println("F학점입니다. 재수강이 필요합니다.");
+			}
+		}
+		System.out.println("종료되었습니다.");
+	}
+	
+}
